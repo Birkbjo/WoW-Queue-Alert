f7f135484d9b50822b254d73d6c8773eacbd211e
Birk Johansson <birk@dhis2.org>
2019-09-02 19:07:05 +0200

    feat(notifier): support all devices

--- notify.js
+++ notify.js
@@ -3,10 +3,9 @@ const config = require('./config.json');
 const Pushbullet = require('pushbullet');
 const inquirer = require('inquirer');
 const fs = require('fs');
-const log = require('ulog')('notifier')
+const log = require('ulog')('notifier');
 
 class Notifier {
-
     async init(argv) {
         if (!config || !config.PUSHBULLET.API_KEY) {
             log.info('Notifier not configured');
@@ -17,7 +16,11 @@ class Notifier {
         this.PB.note = promisify(this.PB.note);
         this.PB.devices = promisify(this.PB.devices);
 
-        if (!config.PUSHBULLET.DEVICE_ID || argv.setup) {
+        if (
+            (!config.PUSHBULLET.DEVICE_ID &&
+                config.PUSHBULLET.DEVICE_ID !== null) ||
+            argv.setup
+        ) {
             await this.setup();
         } else {
             this.device = config.PUSHBULLET.DEVICE_ID;
@@ -27,23 +30,37 @@ class Notifier {
     async setup() {
         try {
             const res = await this.PB.devices();
-
-            if(!res.devices || res.devices.length < 1) {
+            if (!res.devices || res.devices.length < 1) {
                 log.error('No devices found.');
                 return;
             }
+            
             const answer = await inquirer.prompt([
                 {
                     type: 'list',
                     message: 'Select a device to send notifications to:',
                     name: 'device',
-                    choices: res.devices.map(d => ({ name: `${d.nickname} [${d.type}]`, value: d })),
+                    choices: res.devices
+                        .map(d => ({
+                            name: `${d.nickname} [${d.type}]`,
+                            value: d,
+                        }))
+                        .concat({
+                            name: 'All devices',
+                            value: { nickname: 'All devices', iden: null },
+                        }),
                 },
             ]);
             this.device = answer.device.iden;
             config.PUSHBULLET.DEVICE_ID = this.device;
-            fs.writeFileSync('config.json', JSON.stringify(config, null, 4).concat('\n'), 'utf8');
-            log.info(`Device set to: ${answer.device.nickname} (id:${answer.device.iden})`);
+            fs.writeFileSync(
+                'config.json',
+                JSON.stringify(config, null, 4).concat('\n'),
+                'utf8'
+            );
+            log.info(
+                `Device set to: ${answer.device.nickname} (id:${answer.device.iden})`
+            );
         } catch (e) {
             log.error('Failed to get devices: ', e);
         }
@@ -52,8 +69,8 @@ class Notifier {
     async notify(title, body) {
         try {
             const d = await this.PB.note(this.device, title, body);
-            log.debug('Notification sent!')
-        } catch(e) {
+            log.debug('Notification sent!');
+        } catch (e) {
             log.error('Failed to send notification: ', e);
         }
     }
